df482a2ea70d5deb122a4e606f8b47bf26117aef
byHYONS <byhyons@gmail.com>
2024-06-11 10:47:25 +0200

    add about page

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,37 @@
+<!-- JAVASCRIPT -->
+<script>
+// import:
+// import nomeImport from 'nomeImport';
+
+export default {
+   nome: 'AboutPage',
+   props: {
+
+   },
+   data(){
+      return{
+
+      }
+   },
+   components: {
+
+   }
+}
+
+</script>
+
+
+<!-- HTML -->
+<template>
+  <h3>ciao sono l'about page</h3>
+
+</template>
+
+
+<!-- STYLE -->
+<style lang="scss" scoped>
+// import
+@use '../scss/partials/variables' as *;
+
+
+</style>
