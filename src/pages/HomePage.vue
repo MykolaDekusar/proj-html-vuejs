47ee6d6daa79c672fe1be52e7d5ac3311c6c77a0
rinox3005 <peppe.rinox@gmail.com>
2024-06-11 12:18:24 +0200

    [UPD] added basic slider to jumbo section

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,12 +1,43 @@
 <script>
 export default {
   name: "HomePage",
+  data() {
+    return {
+      currentIndex: 0,
+      jumboImages: ["bg1.png", "bg2.png", "bg3.png"],
+      interval: null,
+    };
+  },
+  mounted() {
+    this.startSlider();
+  },
+  beforeDestroy() {
+    clearInterval(this.interval);
+  },
+  methods: {
+    startSlider() {
+      this.interval = setInterval(this.showNextImage, 3000); // Change image every 3 seconds
+    },
+    showNextImage() {
+      this.currentIndex = (this.currentIndex + 1) % this.jumboImages.length;
+    },
+  },
 };
 </script>
 
 <template>
   <main>
-    <div class="jumbo"><h2>Jumbotron</h2></div>
+    <div class="jumbo">
+      <img
+        v-for="(image, index) in jumboImages"
+        :key="index"
+        :src="`/img/background/${image}`"
+        :class="{ active: index === currentIndex }"
+        class="image"
+        alt=""
+      />
+    </div>
+
     <div class="trending-games"><h2>Trending Games</h2></div>
     <div class="experience"><h2>12 Years Experience</h2></div>
     <div class="live"><h2>Live Streaming</h2></div>
@@ -20,58 +51,54 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-.container-xxl {
-  height: 900px;
-}
-
 .jumbo {
   background-color: antiquewhite;
+  text-align: center;
+  position: relative;
+  width: 100%;
+  height: 900px;
+  overflow: hidden;
+
+  .image {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    transition: opacity 1s;
+  }
+
+  .image.active {
+    opacity: 1;
+  }
+}
+
+.trending-games,
+.experience,
+.live,
+.trending,
+.upcoming,
+.shop,
+.team,
+.features,
+.news {
   height: 500px;
   text-align: center;
 }
-.trending-games {
-  background-color: rgb(56, 42, 243);
-  height: 500px;
-  text-align: center;
-}
-.experience {
-  background-color: antiquewhite;
-  height: 500px;
-  text-align: center;
-}
-.live {
-  background-color: rgb(56, 42, 243);
-  height: 500px;
-  text-align: center;
-}
-.trending {
-  background-color: antiquewhite;
-  height: 500px;
-  text-align: center;
-}
-.upcoming {
-  background-color: rgb(56, 42, 243);
-  height: 500px;
-  text-align: center;
-}
-.shop {
-  background-color: antiquewhite;
-  height: 500px;
-  text-align: center;
-}
-.team {
-  background-color: rgb(56, 42, 243);
-  height: 500px;
-  text-align: center;
-}
-.features {
-  background-color: antiquewhite;
-  height: 500px;
-  text-align: center;
-}
+
+.trending-games,
+.live,
+.upcoming,
+.team,
 .news {
   background-color: rgb(56, 42, 243);
-  height: 500px;
-  text-align: center;
+}
+
+.experience,
+.trending,
+.shop,
+.features {
+  background-color: antiquewhite;
 }
 </style>
