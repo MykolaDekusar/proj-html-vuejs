1cc7b78346e771c0d2fb05b923a31aaa2f0ff89b
rinox3005 <peppe.rinox@gmail.com>
2024-06-11 11:02:27 +0200

    [UPD] HomePage layout main sections done

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,7 +5,73 @@ export default {
 </script>
 
 <template>
-  <h1>Home Page</h1>
+  <main>
+    <div class="jumbo"><h2>Jumbotron</h2></div>
+    <div class="trending-games"><h2>Trending Games</h2></div>
+    <div class="experience"><h2>12 Years Experience</h2></div>
+    <div class="live"><h2>Live Streaming</h2></div>
+    <div class="trending"><h2>Trending Battle</h2></div>
+    <div class="upcoming"><h2>Upcoming Match</h2></div>
+    <div class="shop"><h2>Gamers Shop</h2></div>
+    <div class="team"><h2>Team Members</h2></div>
+    <div class="features"><h2>Our Extreme Features</h2></div>
+    <div class="news"><h2>Latest News</h2></div>
+  </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container-xxl {
+  height: 900px;
+}
+
+.jumbo {
+  background-color: antiquewhite;
+  height: 500px;
+  text-align: center;
+}
+.trending-games {
+  background-color: rgb(56, 42, 243);
+  height: 500px;
+  text-align: center;
+}
+.experience {
+  background-color: antiquewhite;
+  height: 500px;
+  text-align: center;
+}
+.live {
+  background-color: rgb(56, 42, 243);
+  height: 500px;
+  text-align: center;
+}
+.trending {
+  background-color: antiquewhite;
+  height: 500px;
+  text-align: center;
+}
+.upcoming {
+  background-color: rgb(56, 42, 243);
+  height: 500px;
+  text-align: center;
+}
+.shop {
+  background-color: antiquewhite;
+  height: 500px;
+  text-align: center;
+}
+.team {
+  background-color: rgb(56, 42, 243);
+  height: 500px;
+  text-align: center;
+}
+.features {
+  background-color: antiquewhite;
+  height: 500px;
+  text-align: center;
+}
+.news {
+  background-color: rgb(56, 42, 243);
+  height: 500px;
+  text-align: center;
+}
+</style>
