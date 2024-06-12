a70992f97eacc6f47f79abbef54cc49b29c4048c
MykolaDekusar <ndekusar97@gmail.com>
2024-06-12 14:57:28 +0200

    [UPD]Optimization to GoogleMaps IFrame settings

--- src/components/GoogleMapsIFrame.vue
+++ src/components/GoogleMapsIFrame.vue
@@ -7,16 +7,13 @@ export default {
 <!-- HTML -->
 <template>
   <section  class="google-map">
-    <div style="width: 100%"><iframe width="100%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=500&amp;hl=en&amp;q=69/6%20Broklyn%20Street,%20New%20York%20city,%20United%20States%20of%20America+()&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
+    <iframe frameborder="0" scrolling="no" width="100%" height="500" border="0" src="https://maps.google.com/maps?width=100%25&amp;height=500&amp;hl=en&amp;q=69/6%20Broklyn%20Street,%20New%20York%20city,%20United%20States%20of%20America+()&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
   </section>
 </template>
-
-<!-- STYLE -->
-<style lang="scss" scoped>
+<style scoped lang="scss">
+@use '../assets/scss/partials/variables' as *;
 .google-map{
-   iframe{
-      width: 100%;
-      height: 500px;
-   }
+    background-color: $bg-btn-secondary;
+    height: 500px;
 }
 </style>
